a61bdfda56a37770efc71536e3f3e04fd88dd084
Eliseo-Villalobos <elibat1000@gmail.com>
2025-06-17 09:09:10 -0600

    Ajustes para el correcto conteo de tiempo de partida

--- src/misClases/Servidor.java
+++ src/misClases/Servidor.java
@@ -14,12 +14,20 @@ import misClases.Conexion;
  * @author Eliba
  */
 public class Servidor extends Conexion{//Se hereda de conexion para el uso de sokcets y generar el  ambiente de servidor
-    public Servidor() throws IOException{super("servidor");}//Se usa el consutructor de la clase padre
+    private JFrameTablero frame;
+    //constructor para que se pueda ejecutar un metodo, ya que startserver como tal no acaba
+    public Servidor(JFrameTablero frame) throws IOException{
+        super("servidor");
+        this.frame = frame;
+    }//Se usa el consutructor de la clase padre
     public void startServer(){//Metodo para iniciar el servidor
         try{
             System.out.println("Esperando..");//se espera la conexion
             cs=ss.accept();//comienza el socket y espera una conexion desde el cliente
             System.out.println("Cliente en linea");
+            if(frame!=null){
+                frame.conexionLista();
+            }
             //se obtiene el flujo de salida del cliente
             salidaCliente = new DataOutputStream(cs.getOutputStream());
             //se le envia un mensaje al cliente usando su flujo de salida
@@ -31,10 +39,11 @@ public class Servidor extends Conexion{//Se hereda de conexion para el uso de so
                 //se muestra por pantalla el mensaje recibido
                 System.out.println(mensajeServidor);
             }
-            System.out.println("Fin de la conexion");
-            ss.close();//finaliza la conexion entre ambas compus
+            //System.out.println("Fin de la conexion");
+            //ss.close();//finaliza la conexion entre ambas compus
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
     }
+    
 }
