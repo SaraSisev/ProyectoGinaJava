e404f699417d9e0d36e8826b225cd4fdfb73e82f
Eliseo-Villalobos <elibat1000@gmail.com>
2025-06-21 19:12:16 -0600

    Modifiaciones para el intercambio de nombres y asi guardarlos

--- src/misClases/Jugador.java
+++ src/misClases/Jugador.java
@@ -18,23 +18,36 @@ import misClases.Conexion;
 public class Jugador extends Conexion{
     boolean jugando = true;//Bandera que representa que el jugador sigue jugando y asi siga mandando mensajes al servidor
     Scanner sc = new Scanner(System.in);//Variable para capturar las respuestas
-    //variable para pasar la informacion al frame del chat
+    private String nombreJugador;
+//variable para pasar la informacion al frame del chat
     private JFrameChat frame;     
 //Constructor
     public Jugador() throws IOException{super("cliente");}
     //constructor que recibe la IP y el frame de chat para abrir
-    public Jugador(String ipServidor, JFrameChat frame) throws IOException{
+    public Jugador(String ipServidor, JFrameChat frame,String nombre) throws IOException{
         super("cliente",ipServidor);
         this.frame = frame;
+        this.nombreJugador = nombre;
     }
    
     
     //Metodo para iniciar al jugador
     public void startJugador() throws IOException{
-        this.frame.setSalida(new DataOutputStream(cs.getOutputStream()));
+            DataOutputStream salida = new DataOutputStream(cs.getOutputStream());
+            DataInputStream entrada = new DataInputStream(cs.getInputStream());
+            this.frame.setSalida(salida);
+
+            // Recibir nombre del servidor
+            String nombreServidor = entrada.readUTF();
+            // Enviar nombre del jugador al servidor 
+            salida.writeUTF(nombreJugador);/* Aquí necesitas obtener el nombre del jugador local */
+            // Pasar nombre del servidor al chat
+            frame.setDatosOponente(nombreServidor);
+            // -----------------------------
+        
            new Thread(() -> {
             try {
-                DataInputStream entrada = new DataInputStream(cs.getInputStream());
+                //DataInputStream entrada = new DataInputStream(cs.getInputStream());
                 while (true) {
                     String mensaje = entrada.readUTF();
                     frame.procesarMensajeRecibido(mensaje);
